af6c3b9a28d3212daa9bb096c62c09ddf8f71d41
Ra0611 <an3611nt@gmail.com>
2021-07-26 19:33:28 +0300

    start

--- webpack2021/src/js/app.js
+++ webpack2021/src/js/app.js
@@ -1,2 +1,91 @@
 import '../scss/app.scss';
 import 'bootstrap'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function searchInsert() {
+//     const value = input.value
+//     let result = resData.data.filter((el) => el.title.startsWith(value))
+//     result.map((item) => {
+//         field.insertAdjacentHTML('beforeend',
+//             `<div>${item.title}</div>`
+//         )
+//     })
+// }
+
+// btn.onclick = function () {
+//     searchInsert()
+// }
+// input.addEventListener("keydown", function (event) {
+//     if (event.keyCode === 13) {
+//         searchInsert()
+//     }
+// })
---
+++ webpack2021/src/js/data.js
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export let resData = []
+// export const fetchData = async () => {
+//     const response = await fetch('http://127.0.0.1:3003/testing')
+//     resData = await response.json()
+//     return response
+
+// }
